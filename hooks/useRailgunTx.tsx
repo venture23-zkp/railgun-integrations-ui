b5d71f3acf45ccc5b552271690eef7d7a5e16604
bbist <pro.bbist@gmail.com>
2023-05-26 01:59:10 +0545

    add executeRecipe method

--- hooks/useRailgunTx.tsx
+++ hooks/useRailgunTx.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import { parseUnits } from '@ethersproject/units';
+import { Recipe, RecipeInput } from '@railgun-community/cookbook';
 import {
+  gasEstimateForUnprovenCrossContractCalls,
   gasEstimateForUnprovenTransfer,
   gasEstimateForUnprovenUnshield,
   gasEstimateForUnprovenUnshieldBaseToken,
+  generateCrossContractCallsProof,
   generateTransferProof,
   generateUnshieldBaseTokenProof,
   generateUnshieldProof,
+  populateProvedCrossContractCalls,
   populateProvedTransfer,
   populateProvedUnshield,
   populateProvedUnshieldBaseToken,
@@ -21,15 +25,16 @@ import {
   RailgunERC20AmountRecipient,
   TransactionGasDetailsSerialized,
   deserializeTransaction,
+  serializeUnsignedTransaction,
 } from '@railgun-community/shared-models';
 import { ethers } from 'ethers';
+import { isAddress } from 'ethers/lib/utils.js';
 import { useSigner } from 'wagmi';
 import { useNetwork } from 'wagmi';
 import { useRailgunWallet } from '@/contexts/RailgunWalletContext';
 import useShieldPrivateKey from '@/hooks/useShieldPrivateKey';
 import { ethAddress } from '@/utils/constants';
 import { getNetwork } from '@/utils/networks';
-import { isAddress } from 'ethers/lib/utils.js';
 
 export type TokenTransferType = {
   address: string;
@@ -146,12 +151,8 @@ const useRailgunTx = () => {
     }
   };
 
-  const unshieldBaseToken = async ({
-    amount,
-    recipient,
-    decimals,
-  }: TokenTransferType) => {
-    console.log("unshielding base");
+  const unshieldBaseToken = async ({ amount, recipient, decimals }: TokenTransferType) => {
+    console.log('unshielding base');
     const wrappedERC20Amount: RailgunERC20Amount = {
       tokenAddress: wethAddress, // wETH
       amountString: parseUnits(amount!, decimals).toHexString(), // hexadecimal amount
@@ -251,7 +252,7 @@ const useRailgunTx = () => {
       throw new Error(populateProvedUnshieldBaseTokenError || 'No serializedTransaction!');
     }
 
-    console.log("nullifiers:", nullifiers);
+    console.log('nullifiers:', nullifiers);
 
     const nonce = await signer?.getTransactionCount();
 
@@ -264,22 +265,18 @@ const useRailgunTx = () => {
     return await signer!.sendTransaction(transactionRequest);
   };
 
-  const unshieldToken = async ({
-    address,
-    amount,
-    recipient,
-    decimals,
-  }: TokenTransferType) => {
-
+  const unshieldToken = async ({ address, amount, recipient, decimals }: TokenTransferType) => {
     if (!isAddress(recipient)) {
       throw new Error(`Not a valid public 0x address: '${recipient}'`);
     }
 
-    const erc20AmountRecipients: RailgunERC20AmountRecipient[] = [{
-      tokenAddress: address, // wETH
-      amountString: parseUnits(amount!, decimals).toHexString(), // hexadecimal amount
-      recipientAddress: recipient,
-    }];
+    const erc20AmountRecipients: RailgunERC20AmountRecipient[] = [
+      {
+        tokenAddress: address, // wETH
+        amountString: parseUnits(amount!, decimals).toHexString(), // hexadecimal amount
+        recipientAddress: recipient,
+      },
+    ];
 
     // Gas price, used to calculate Relayer Fee iteratively.
     const feeData = await signer?.getFeeData();
@@ -371,7 +368,7 @@ const useRailgunTx = () => {
       throw new Error(populateProvedUnshieldError || 'No serializedTransaction!');
     }
 
-    console.log("nullifiers:", nullifiers);
+    console.log('nullifiers:', nullifiers);
 
     const nonce = await signer?.getTransactionCount();
 
@@ -383,7 +380,7 @@ const useRailgunTx = () => {
     // send transactionRequest to Relay.sol
     return await signer!.sendTransaction(transactionRequest);
   };
-  
+
   const transfer = async (args: TokenTransferType) => {
     setIsExecuting(true);
     try {
@@ -507,7 +504,7 @@ const useRailgunTx = () => {
       throw new Error(populateProvedTransferError || 'No serializedTransaction!');
     }
 
-    console.log("nullifiers:", nullifiers);
+    console.log('nullifiers:', nullifiers);
 
     const nonce = await signer?.getTransactionCount();
 
@@ -522,7 +519,147 @@ const useRailgunTx = () => {
     return await signer!.sendTransaction(transactionRequest);
   };
 
-  return { shield, unshield, transfer, isExecuting, shieldPrivateKey };
+  const executeRecipe = async (recipe: Recipe, input: RecipeInput) => {
+    const {
+      networkName,
+      erc20Amounts: inputERC20Amounts,
+      nfts: relayAdaptUnshieldNFTAmounts,
+    } = input;
+
+    const recipeOutput = await recipe.getRecipeOutput(input);
+    console.log(recipeOutput);
+
+    const {
+      populatedTransactions,
+      erc20Amounts,
+      nfts: relayAdaptShieldNFTs,
+    } = recipeOutput;
+    
+    const crossContractCallsSerialized = populatedTransactions.map(serializeUnsignedTransaction);
+
+    const relayAdaptUnshieldERC20Amounts: RailgunERC20Amount[] = inputERC20Amounts.map(
+      ({ tokenAddress, amount }) => {
+        return { tokenAddress, amountString: amount.toHexString() };
+      }
+    );
+
+    const relayAdaptShieldERC20Addresses = erc20Amounts.map(({ tokenAddress }) => tokenAddress);
+
+    // Gas price, used to calculate Relayer Fee iteratively.
+    const feeData = await signer?.getFeeData();
+    const originalGasDetailsSerialized: TransactionGasDetailsSerialized = {
+      evmGasType: EVMGasType.Type2, // Depends on the chain (BNB uses type 0)
+      gasEstimateString: '0x00', // Always 0, we don't have this yet.
+      maxFeePerGasString: feeData?.maxFeePerGas?.toHexString() || '0x100000', // Current gas Max Fee
+      maxPriorityFeePerGasString: feeData?.maxPriorityFeePerGas?.mul(2).toHexString() || '0x010000', // Current gas Max Priority Fee
+    };
+    const feeTokenDetails = undefined;
+
+    // Whether to use a Relayer or self-signing wallet.
+    // true for self-signing, false for Relayer
+    const sendWithPublicWallet = true;
+
+    const { gasEstimateString, error: gasEstimateForUnprovenCrossContractCallsError } =
+      await gasEstimateForUnprovenCrossContractCalls(
+        networkName,
+        wallet?.id!,
+        encryptionKey!,
+        relayAdaptUnshieldERC20Amounts,
+        relayAdaptUnshieldNFTAmounts,
+        relayAdaptShieldERC20Addresses,
+        relayAdaptShieldNFTs,
+        crossContractCallsSerialized,
+        originalGasDetailsSerialized,
+        feeTokenDetails,
+        sendWithPublicWallet
+      );
+    if (gasEstimateForUnprovenCrossContractCallsError || gasEstimateString === undefined) {
+      throw new Error(gasEstimateForUnprovenCrossContractCallsError || 'No gasEstimateString!');
+    }
+
+    const relayerFeeERC20AmountRecipient = undefined;
+
+    // Minimum gas price, only required for relayed transaction.
+    const overallBatchMinGasPrice: string = '0x10000';
+
+    const progressCallback = (progress: number) => {
+      // Handle proof progress (show in UI).
+      // Proofs can take 20-30 seconds on slower devices.
+      console.log('progress', progress);
+    };
+
+    const { error: generateCrossContractCallsProofError } = await generateCrossContractCallsProof(
+      networkName,
+      wallet?.id!,
+      encryptionKey!,
+      relayAdaptUnshieldERC20Amounts,
+      relayAdaptUnshieldNFTAmounts,
+      relayAdaptShieldERC20Addresses,
+      relayAdaptShieldNFTs,
+      crossContractCallsSerialized,
+      relayerFeeERC20AmountRecipient,
+      sendWithPublicWallet,
+      overallBatchMinGasPrice,
+      progressCallback
+    );
+    if (generateCrossContractCallsProofError) {
+      // Proof generated successfully.
+      throw new Error(generateCrossContractCallsProofError);
+    }
+
+    // NOTE: Must follow proof generation.
+    // Use the exact same parameters as proof or this will throw invalid error.
+
+    // // Gas to use for the transaction.
+    // const gasDetailsSerialized: TransactionGasDetailsSerialized = {
+    //   evmGasType: EVMGasType.Type2, // Depends on the chain (BNB uses type 0)
+    //   gasEstimateString: "0x0100", // Output from gasEstimateForDeposit
+    //   maxFeePerGasString: "0x100000", // Current gas Max Fee
+    //   maxPriorityFeePerGasString: "0x010000", // Current gas Max Priority Fee
+    // };
+    const gasDetailsSerialized: TransactionGasDetailsSerialized = {
+      ...originalGasDetailsSerialized,
+      gasEstimateString,
+    };
+
+    const {
+      nullifiers,
+      serializedTransaction,
+      error: populateProvedCrossContractCallsError,
+    } = await populateProvedCrossContractCalls(
+      networkName,
+      wallet?.id!,
+      relayAdaptUnshieldERC20Amounts,
+      relayAdaptUnshieldNFTAmounts,
+      relayAdaptShieldERC20Addresses,
+      relayAdaptShieldNFTs,
+      crossContractCallsSerialized,
+      relayerFeeERC20AmountRecipient,
+      sendWithPublicWallet,
+      overallBatchMinGasPrice,
+      gasDetailsSerialized
+    );
+    if (populateProvedCrossContractCallsError || serializedTransaction === undefined) {
+      // Error populating transaction.
+      throw new Error(populateProvedCrossContractCallsError || 'No serializedTransaction!');
+    }
+
+    console.log('nullifiers:', nullifiers);
+
+    const nonce = await signer?.getTransactionCount();
+
+    const transactionRequest = deserializeTransaction(serializedTransaction, nonce, chainId);
+
+    // Public wallet to shield from.
+    transactionRequest.from = await signer?.getAddress();
+
+    console.log(transactionRequest);
+
+    // send transactionRequest to Relay.sol
+    return await signer!.sendTransaction(transactionRequest);
+  };
+
+  return { isExecuting, shieldPrivateKey, shield, unshield, transfer, executeRecipe };
 };
 
 export default useRailgunTx;
