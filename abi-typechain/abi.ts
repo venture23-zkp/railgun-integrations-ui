a4892437288b00aab89bc06f8f1ff9449f77f4f0
illusioncoder7 <shubh.tiwari512@gmail.com>
2023-06-01 16:38:09 +0545

    add registry and account

--- abi-typechain/abi.ts
+++ abi-typechain/abi.ts
@@ -1,6 +1,10 @@
 import ABI_ACM from './acm/ACM.json';
+import ABI_ERC6551Registry from './ERC6551Registry/ERC6551Registry.json';
+import ABI_Default_Account from './DefaultERC6551Account/DefaultERC6551Account.json';
 
 
 export const abi = {
-    ACM: ABI_ACM
+    ACM: ABI_ACM,
+    ERC6551_Registry:ABI_ERC6551Registry,
+    Default_Account:ABI_Default_Account
 } as const;
