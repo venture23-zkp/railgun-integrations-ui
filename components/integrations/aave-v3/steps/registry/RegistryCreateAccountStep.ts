ae56f0993c9c926b5f9ac70bbb804941d2f70919
illusioncoder7 <shubh.tiwari512@gmail.com>
2023-06-02 17:39:23 +0545

    reafactor nft details

--- components/integrations/aave-v3/steps/registry/RegistryCreateAccountStep.ts
+++ components/integrations/aave-v3/steps/registry/RegistryCreateAccountStep.ts
@@ -4,6 +4,7 @@ import { NFTTokenType } from '@railgun-community/shared-models';
 import { ERC6551_Registry } from '@/contract/erc-6551-registry-contract';
 import { Address } from '../../utils/address';
 import { BytesLike } from 'ethers';
+import { ZERO_ADDRESS } from '@railgun-community/engine';
 
 export type RegistryCreateAccountData = {
     implementation: string;
@@ -32,7 +33,18 @@ export class RegistryCreateAccountStep extends Step {
     protected async getStepOutput(input: StepInput): Promise<UnvalidatedStepOutput> {
         const contract = new ERC6551_Registry(this.registry);
         const { implementation, chainId, tokenContract, tokenId, salt, initData } = this.data;
+        var tokenSubID,nftAddress;
         const populatedTransaction = await contract.createCreateAccount(implementation, chainId, tokenContract, tokenId, salt, initData);
+// TODO: if case for nft
+        if (tokenContract == ZERO_ADDRESS && tokenId == BigNumber.from(0)){
+            nftAddress = this.registry;
+            tokenSubID = 0;
+
+        }
+        else {
+            tokenSubID = tokenId;
+            nftAddress = tokenContract;
+        }
 
         return {
             populatedTransactions: [populatedTransaction],
@@ -43,8 +55,8 @@ export class RegistryCreateAccountStep extends Step {
                 ...input.nfts,
                 {
                     nftTokenType: NFTTokenType.ERC721,
-                    nftAddress:this.registry,
-                    tokenSubID: tokenId,
+                    nftAddress:nftAddress,
+                    tokenSubID: String(tokenSubID),
                     amountString: '1',
                 },
             ],
