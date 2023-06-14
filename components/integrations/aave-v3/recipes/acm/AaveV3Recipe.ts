e5553b2fae2ff1af873b6163167b26029a7985af
samman adhikari <sammanadh@pop-os.localdomain>
2023-06-14 15:32:20 +0545

    AaveV3Recipe supportsNetwork fixed

--- components/integrations/aave-v3/recipes/acm/AaveV3Recipe.ts
+++ components/integrations/aave-v3/recipes/acm/AaveV3Recipe.ts
@@ -8,17 +8,26 @@ export abstract class AaveV3Recipe extends Recipe {
   };
 
   protected supportsNetwork(networkName: NetworkName): boolean {
+    console.log(networkName === NetworkName.PolygonMumbai)
     switch (networkName) {
       case NetworkName.EthereumGoerli:
         return true;
       case NetworkName.Ethereum:
+        return true;
       case NetworkName.Polygon:
+        return true;
       case NetworkName.Arbitrum:
+        return true;
       case NetworkName.Railgun:
+        return true;
       case NetworkName.EthereumRopsten_DEPRECATED:
+        return true;
       case NetworkName.PolygonMumbai:
+        return true;
       case NetworkName.ArbitrumGoerli:
+        return true;
       case NetworkName.Hardhat:
+        return true;
       default:
         throw new Error('Chain not supported by Aave');
     }
