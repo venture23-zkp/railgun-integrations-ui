1e80913d0bde8f54f3bede79ef35cb6d76f54a6e
samman adhikari <sammanadh@pop-os.localdomain>
2023-06-19 16:12:37 +0545

    filtering aave tokens based on network id

--- hooks/useAaveTokenList.tsx
+++ hooks/useAaveTokenList.tsx
@@ -1,51 +1,54 @@
 import { useNetwork } from 'wagmi';
 import aTokenListJson from '@/public/aTokenList.json';
-import dVariableTokenList from '@/public/dVariableTokenList.json';
 import dStableTokenList from '@/public/dStableTokenList.json';
+import dVariableTokenList from '@/public/dVariableTokenList.json';
 import { TOKEN_PRIORITY_SORT } from '@/utils/constants';
 
 export interface AaveTokenListItem {
-    chainId: number;
-    symbol: string;
-    address: string;
-    decimals: number;
-    name: string;
-    logoURI: string;
-    originalTokenAddress: string;
+  chainId: number;
+  symbol: string;
+  address: string;
+  decimals: number;
+  name: string;
+  logoURI: string;
+  originalTokenAddress: string;
 }
 
 export const useAaveTokenList = () => {
-    const { chain } = useNetwork();
+  const { chain } = useNetwork();
 
-    const aTokenList = aTokenListJson.tokens
-        .sort((a, b) => {
-            // sorts most common tokens to the top of the tokenList
-            for (const symbol of TOKEN_PRIORITY_SORT) {
-                if (b.symbol === symbol) return 1;
-                if (a.symbol === symbol) return -1;
-            }
-            return 0;
-        });
+  const aTokenList = aTokenListJson.tokens
+    .filter((token) => token.chainId === chain?.id)
+    .sort((a, b) => {
+      // sorts most common tokens to the top of the tokenList
+      for (const symbol of TOKEN_PRIORITY_SORT) {
+        if (b.symbol === symbol) return 1;
+        if (a.symbol === symbol) return -1;
+      }
+      return 0;
+    });
 
-    const dVariableToken = dVariableTokenList.tokens
-        .sort((a, b) => {
-            // sorts most common tokens to the top of the tokenList
-            for (const symbol of TOKEN_PRIORITY_SORT) {
-                if (b.symbol === symbol) return 1;
-                if (a.symbol === symbol) return -1;
-            }
-            return 0;
-        });
+  const dVariableToken = dVariableTokenList.tokens
+    .filter((token) => token.chainId === chain?.id)
+    .sort((a, b) => {
+      // sorts most common tokens to the top of the tokenList
+      for (const symbol of TOKEN_PRIORITY_SORT) {
+        if (b.symbol === symbol) return 1;
+        if (a.symbol === symbol) return -1;
+      }
+      return 0;
+    });
 
-    const dStableToken = dStableTokenList.tokens
-        .sort((a, b) => {
-            // sorts most common tokens to the top of the tokenList
-            for (const symbol of TOKEN_PRIORITY_SORT) {
-                if (b.symbol === symbol) return 1;
-                if (a.symbol === symbol) return -1;
-            }
-            return 0;
-        });
+  const dStableToken = dStableTokenList.tokens
+    .filter((token) => token.chainId === chain?.id)
+    .sort((a, b) => {
+      // sorts most common tokens to the top of the tokenList
+      for (const symbol of TOKEN_PRIORITY_SORT) {
+        if (b.symbol === symbol) return 1;
+        if (a.symbol === symbol) return -1;
+      }
+      return 0;
+    });
 
-    return { aTokenList, dVariableToken, dStableToken };
+  return { aTokenList, dVariableToken, dStableToken };
 };
